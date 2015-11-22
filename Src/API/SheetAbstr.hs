c11351544c58eb2dea0a701a4c618818f24addb2
Rick Klomp <rick93nl@gmail.com>
2015-11-22 15:01:48 +0100

    Updated comments in the API

--- Src/API/SheetAbstr.hs
+++ Src/API/SheetAbstr.hs
@@ -27,30 +27,22 @@ type Env v e = Map v e
 
 
 -- | The Spreadsheet API interface supplies toplevel functions.
-class (MonadState s m, Var v, Expr e v (Reader (Env v e)), Cell c e v (Reader (Env v e))) => Spreadsheet s c e v m | s -> c, s -> v, s -> e, s -> m where
+class (MonadState s m, Var v, Expr e v (Reader (Map v e)), Cell c e v (Reader (Map v e))) => Spreadsheet s c e v m | s -> c, s -> v, s -> e, s -> m where
   -- | 'updateEvals' performs a full update (it evaluates each cell).
-  -- The current implementation that has been used to experiment with this
-  -- API performs naive updates. It might be a good idea to change this
-  -- part of the API to allow for other update strategies being used
-  -- (updateEvals currently doesn't accept a list of changes that it has to
-  -- act upon). Although other update strategies could be applied with the
-  -- current API as well by saving additional information inside the
-  -- Spreadsheet datatype 's'.
   updateEvals :: m ()
   -- | 'getCell' retrieves a cell from the spreadsheet.
   getCell :: Pos -> m (Maybe c)
-  -- | 'setCell' sets a 'Cell' c at 'Pos' in the spreadsheet. If a 'Cell' at the given 'Pos' was already present, it is overwritten.
+  -- | 'setCell' sets a 'Cell' c at 'Pos' in the spreadsheet.
+  -- If a 'Cell' at the given 'Pos' was already present, it is overwritten.
   setCell :: Pos -> c -> m ()
 
 -- | The 'Cell' API interface supplies cell manipulation functions.
-class (MonadReader (Env v e) m, Var v, Expr e v m) => Cell c e v m | c -> e, c -> v, v -> m, e -> m where
+class (MonadReader (Map v e) m, Var v, Expr e v m) => Cell c e v m | c -> e, c -> v, v -> m, e -> m where
   -- | 'evalCell' tries to evaluate the expression that it contains.
   -- Prior to calling this, the cell's textual contents need to have been
-  -- parsed using the 'parseCell' function.
-  --
-  ---- | 'setGlobalVars' sets the global variables 'v' along with their
-  -- definitions 'e'. Subsequent calls to 'evalCell' will be able to use
-  -- this.
+  -- parsed using the 'parseCell' function. This is run under a MonadReader
+  -- environment, where values of required global variables should be
+  -- available to read.
   evalCell :: c -> m c
   -- | 'parseCell' Tries to parse the textual contents of 'Cell' c.
   parseCell :: c -> c
@@ -64,12 +56,10 @@ class (MonadReader (Env v e) m, Var v, Expr e v m) => Cell c e v m | c -> e, c -
 
 
 -- | The 'Expr' API interface supplies expression manipulation functions.
-class (MonadReader (Env v e) m, Var v) => Expr e v m | e -> v, v -> m, e -> m where
-  -- | 'evalExpr' evaluates the expression. Currently this part of the API
-  -- expects that succesfully evaluating an expression will result in
-  -- another expression of the same language. It might be desirable to
-  -- change this function\'s type signature should this expection be(come)
-  -- invalid.
+class (MonadReader (Map v e) m, Var v) => Expr e v m | e -> v, v -> m, e -> m where
+  -- | 'evalExpr' evaluates the expression. This function is run under a MonadReader
+  -- environment, where values of required global variables should be
+  -- available to read.
   evalExpr :: e -> m e
 
 -- | The 'Var' API interface is currently purely used to allow for different
