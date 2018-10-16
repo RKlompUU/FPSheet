f2064b1002d2dd4ce7d8874d391b4515ebfd7cbb
Rick Klomp <r.klomp@students.uu.nl>
2018-10-16 14:52:13 +0100

    Update Types.hs
    Types modified of the standard implementation to adhere to the new abstraction definition

--- lib/Sheet/Backend/Standard/Types.hs
+++ lib/Sheet/Backend/Standard/Types.hs
@@ -12,17 +12,20 @@ import qualified Data.Map as M
 
 import Sheet.Backend.SheetAbstr
 
+
+-- | Each Cell is positioned on a 2-dimensional grid. Its position is
+-- defined by the ith col and jth row, where i and j are \"(i, j) :: 'Pos'\"
+type Pos = (Int,Int)
+
 data CellT e =
   CellT { cStr   :: String -- |User defined cell's text
-        , cExpr  :: Maybe (e) -- |The result from parsing (and executing?) str
+        , cRes   :: Maybe e -- |The result of the last evaluation of cStr
         , cUFlag :: Bool -- |Cell has changed, used to check if an input field needs to be refreshed by the frontend
+        , cPos   :: Pos
   } deriving (Show)
 
 type Sheet c = Map Pos c
 
-data ExprT v =
-  ExprT {
-    evaluatedExpr :: String
-  } deriving (Show, Eq)
+type ExprT v = String
 
 type VarT = String
